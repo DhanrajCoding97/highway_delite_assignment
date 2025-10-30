b2861e8c08f0a1b7605afb98e917f0fcea4a7038
dhanraj shetty <dhanrajcoding@gmail.com>
2025-10-30 14:38:58 +0530

    added tailwind css and shadcn

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** Dummy config for ShadCN until official Tailwind v4 support */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: { extend: {} },
+  plugins: []
+};
