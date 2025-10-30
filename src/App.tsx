1ee484149327c62b7b1c74413d76124b4847ecd7
dhanraj shetty <dhanrajcoding@gmail.com>
2025-10-30 17:58:59 +0530

    addded experience details view component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import ExperienceCard from './components/ExperienceCard';
 import SearchInput from './components/searchInput';
+import ExperienceDetails from './components/ExpereienceDetails';
 function App() {
   return (
     <main>
       <Navbar />
       <ExperienceCard />
       <SearchInput />
+      <ExperienceDetails />
     </main>
   );
 }
